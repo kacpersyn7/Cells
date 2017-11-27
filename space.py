c4fbc81082cb4e1f65192aa12d77bae00c298110
kacpersyn <kacper@student.agh.edu.pl>
2017-11-28 00:20:21 +0100

    First ideas

---
+++ space.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy import optimize
+from pyswarm import pso
+
+
+class area:
+    def __init__(self, x_size, y_size, alpha, space_cord, wall_coord, people_coord):
+        self.x_size = x_size
+        self.y_size = y_size
+        self.alpha = alpha
+        self.space_cord = space_cord
+        self.wall_coord = wall_coord
+        self.people_coord = people_coord
+
+    def cells_conditions(self):
+        pass
+
+
+# numpy matrix with coords of walls
+# numpy matrix with coords of cells
+# numpy matrix with coords of people
+# numpy matrix with values of every area point
+# conditions for cells coords
+# idea - import from bitmap file
+def update_value(area, cells_coord):
+    pass
