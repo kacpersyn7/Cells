46f0e3f38daa8524fef77d3e053c38abf07cbfe6
kacpersyn <kacper@student.agh.edu.pl>
2017-11-29 01:33:49 +0100

    mam pomysl, ale rano napisze

--- building.py
+++ building.py
@@ -1,17 +1,23 @@
 import numpy as np
 
-materials = {"brick": 0.1, "airbrick": 0.2 }
+materials = {"brick": 0.1, "airbrick": 0.2}
 
 
 class Wall:
     def __init__(self, material, vector_coord):
         self.material = material
         self.vector_coord = vector_coord
+        self.alpha = materials[material]
+
+    def get_jamming(self, point):
+        pass
 
 
 class Building:
     walls = np.array([])
 
-    def __init__(self, walls, material):
-        self.walls = walls
-        self.material = material
+    def __init__(self, walls_vector):
+        self.walls_vector = walls_vector
+
+    def jamming(self, point):
+        pass
