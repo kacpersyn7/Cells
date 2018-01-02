8d219c13753653b11c5a991ea07e3c3a93a4cd54
kacpersyn <kacper@student.agh.edu.pl>
2018-01-03 00:32:30 +0100

    New idea

--- accesspoint.py
+++ accesspoint.py
@@ -1,15 +1,31 @@
 import numpy as np
 
+import globals as g
+
 
 class AccessPoint:
     def __init__(self, cost=100, max_power=10, access_area_fun=lambda x, y: x * x + y * y <= 16,
-                 dist_fun=lambda diff_x, diff_y: (diff_x * diff_x + diff_y * diff_y).astype(float)):
+                 dist_fun=lambda diff_x, diff_y: (diff_x * diff_x + diff_y * diff_y).astype(float), x_size=g.COLS,
+                 y_size=g.ROWS):
         self.max_power = max_power
         self.access_area_fun = access_area_fun
         self.dist_fun = dist_fun
         self.cost = cost
+        self.x_size = x_size
+        self.y_size = y_size
 
     def get_power(self, ap_x, ap_y, ue_x, ue_y):
         distance = self.dist_fun(ue_x - ap_x, ue_y - ap_y)
         distance[np.where(distance == 0)[0]] = 0.5
         return (1. / distance) * self.max_power
+
+    def update_result_area(self, ap_bitmap, result_area):
+        access_points = np.where(ap_bitmap == 1)
+        new_result_area = np.copy(result_area)
+        for x, y in zip(access_points[0], access_points[1]):
+            temp_y, temp_x = np.ogrid[-x:self.x_size - x, -y:self.y_size - y]
+            mask_indexes = np.where(self.access_area_fun(temp_x, temp_y))
+            new_result_area[mask_indexes] = self.get_power(x, y, mask_indexes[0], mask_indexes[1])
+            diff = np.where((new_result_area - result_area) < 0)
+            new_result_area[diff] = result_area[diff]
+            result_area = np.copy(new_result_area)
--- particle.py
+++ particle.py
@@ -1,42 +1,42 @@
-import globals as g
 from accesspoint import *
 
 
 ##For more than one type of accesspoint
 
 class Particle:
-    def __init__(self, dimension, x_size=g.COLS, y_size=g.ROWS):
+    def __init__(self, dimension, space, x_size=g.COLS, y_size=g.ROWS):
         self.x_size = x_size
         self.y_size = y_size
+        self.space = space
+        # pointer to accesspoint types
+        # great idea
         self.dimension = dimension
         self.result_area = np.zeros((x_size, y_size))
         self.x_bitmap = np.zeros((dimension, x_size, y_size))
         self.p_bitmap = np.zeros((dimension, x_size, y_size))
+        self.velocity = np.zeros((dimension, x_size, y_size))
         self.x_target = 0.0
-        # self.access_points = [AccessPoint(**x) for x in access_points_types]
 
     def generate_first_individual(self, probability):
         for i in range(self.dimension):
             self.x_bitmap[i] = np.logical_not(
                 np.logical_or(np.random.randint(0, probability[i], size=(self.x_size, self.y_size)), 0)).astype(int)
 
-    def update_result_area(self, access_area_fun, get_power, dim):
-        access_points = np.where(self.x_bitmap[dim] == 1)
-        new_result_area = np.copy(self.result_area)
-        for x, y in zip(access_points[0], access_points[1]):
-            temp_y, temp_x = np.ogrid[-x:self.x_size - x, -y:self.y_size - y]
-            mask_indexes = np.where(access_area_fun(temp_x, temp_y))
-            new_result_area[mask_indexes] = get_power(x, y, mask_indexes[0], mask_indexes[1])
-            diff = np.where((new_result_area - self.result_area) < 0)
-            new_result_area[diff] = self.result_area[diff]
-            self.result_area = np.copy(new_result_area)
+    def update_result_area(self):
+        for i in range(self.dimension):
+            self.space[i].update_result_area(self.x_bitmap[i], self.result_area)
 
     def target_function(self, users_area, costs):
+        self.update_result_area()
         total_aps = np.sum(self.x_bitmap, axis=(1, 2))
         total_cost = total_aps.dot(costs)
         new_area = users_area - self.result_area
         self.x_target = np.sum(new_area[np.where(new_area > 0)]) - total_cost
         return self.x_target
+
+    def calculate_velocity(self, global_best_bitmap, omega=1, fi_p=1, fi_g=1):
+        self.velocity = omega * self.velocity + fi_p * (self.p_bitmap - self.x_bitmap) + fi_g * (
+                global_best_bitmap - self.x_bitmap)
 
     def get_result_area(self):
         return self.result_area
