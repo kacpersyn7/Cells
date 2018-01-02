55c462d9d8b27c0afea247f392121b9129aa1911
kacpersyn <kacper@student.agh.edu.pl>
2018-01-02 23:17:29 +0100

    PSO beginning

--- population.py
+++ population.py
@@ -1,8 +1,8 @@
-from individual import *
+from particle import *
 from userequipment import *
 
 
-class Individuals:
+class Population:
     def __init__(self, population_size=100, access_points_types=g.access_points_types, x_size=g.COLS, y_size=g.ROWS):
         self.x_size = x_size
         self.y_size = y_size
@@ -10,9 +10,10 @@ class Individuals:
         self.dimension = len(access_points_types)
         self.access_points = [AccessPoint(**x) for x in access_points_types]
         self.costs = np.array([x.cost for x in self.access_points])
-        self.individuals_list = np.array([Individual(self.dimension) for i in range(population_size)])
+        self.individuals_list = np.array([Particle(self.dimension) for i in range(population_size)])
         self.global_best = 0
         self.global_best_id = 0
+        self.people = np.random.randint(0, 250, size=(self.x_size, self.y_size))
 
     def generate_first_population(self, low=1, high=100):
         random_list = np.random.uniform(low, high, size=(self.population_size, self.dimension))
@@ -31,5 +32,10 @@ class Individuals:
 
     def calculate_target_function(self):
         self.update_every_individual()
-        self.global_best = max(individual.target_function(self.costs) for individual in self.individuals_list)
-        self.global_best_id = np.where(self.individuals_list == self.global_best)
+        self.global_best = max(
+            individual.target_function(self.people, self.costs) for individual in self.individuals_list)
+        self.global_best_id = \
+        np.where(self.individuals_list == max(self.individuals_list, key=lambda x: x.target_value))[0]
+
+    def get_individual(self, i):
+        return self.individuals_list[i]
