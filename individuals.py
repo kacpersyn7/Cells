54a940dab2c5762b41736377f04b505d52caef09
kacpersyn <kacper@student.agh.edu.pl>
2018-01-02 19:32:30 +0100

    Target function.

--- individuals.py
+++ individuals.py
@@ -9,7 +9,10 @@ class Individuals:
         self.population_size = population_size
         self.dimension = len(access_points_types)
         self.access_points = [AccessPoint(**x) for x in access_points_types]
-        self.individuals_list = [Individual(self.dimension) for i in range(population_size)]
+        self.costs = np.array([x.cost for x in self.access_points])
+        self.individuals_list = np.array([Individual(self.dimension) for i in range(population_size)])
+        self.global_best = 0
+        self.global_best_id = 0
 
     def generate_first_population(self, low=1, high=100):
         random_list = np.random.uniform(low, high, size=(self.population_size, self.dimension))
@@ -18,6 +21,15 @@ class Individuals:
             self.update_individual(i)
 
     def update_individual(self, i):
-        for j in range(self.dimension):
-            self.individuals_list[i].update_result_area(self.access_points[j].access_area_fun,
-                                                        self.access_points[j].get_power, j)
+        for ap_id in range(self.dimension):
+            self.individuals_list[i].update_result_area(self.access_points[ap_id].access_area_fun,
+                                                        self.access_points[ap_id].get_power, ap_id)
+
+    def update_every_individual(self):
+        for i in range(self.population_size):
+            self.update_individual(i)
+
+    def calculate_target_function(self):
+        self.update_every_individual()
+        self.global_best = max(individual.target_function(self.costs) for individual in self.individuals_list)
+        self.global_best_id = np.where(self.individuals_list == self.global_best)
--- userequipment.py
+++ userequipment.py
@@ -7,8 +7,5 @@ class UserEquipment:
     def __init__(self, max_request, x_size=g.COLS, y_size=g.ROWS):
         self.x_size = x_size
         self.y_size = y_size
-        self.area = np.zeros((x_size, y_size))
-        return self
-
-    def get_random(self):
         self.area = np.random.randint(0, self.max_request, size=(self.x_size, self.y_size))
+        self.max_request = max_request
