2c57a5ce0c75bf4a83f42af9da08e43393301db0
kacpersyn <kacper@student.agh.edu.pl>
2018-01-06 23:44:05 +0100

    Działa, ale fatalnie

--- particle.py
+++ particle.py
@@ -12,36 +12,34 @@ class Particle:
         # self.x_bitmap = np.random.randint(lb, up + 1, size=(self.dimension, self.x_size, self.y_size))
         # self.velocity = np.random.randint(-abs(up - lb), abs(up - lb) + 1,
         #                                   size=(self.dimension, self.x_size, self.y_size))
-        self.x_bitmap = self.generate_first_individual(x_randoms)
-        self.velocity = self.generate_first_individual(velocity_randoms)
+        self.x_bitmap = self.init_particle(x_randoms)
+        self.velocity = np.zeros((self.dimension, self.x_size, self.y_size))
         self.p_bitmap = np.copy(self.x_bitmap)
         self.x_target = self.target_func(self.x_bitmap)
         self.p_target = self.x_target
 
-    def generate_first_individual(self, number_of_aps):
-        temp_array = np.zeros(self.x_size * self.y_size)
+    def init_particle(self, number_of_aps):
+        temp_multi_array = np.zeros((self.dimension, self.x_size, self.y_size))
         for i in range(self.dimension):
+            temp_array = np.zeros(self.x_size * self.y_size)
             print(number_of_aps[i])
             temp_array[:number_of_aps[i]] = 1
             np.random.shuffle(temp_array)
-            temp_array = np.zeros(self.x_size * self.y_size)
-        return temp_array.reshape(self.x_size, self.y_size)
+            temp_multi_array[i] = temp_array.reshape(self.x_size, self.y_size)
+        return temp_multi_array
 
     def get_local_best(self):
         return self.p_bitmap, self.p_target
 
-    def iterate(self, global_best, omega=0.5, phip=0.5, phig=0.5):
-        self.calculate_velocity(global_best, omega, phip, phig)
+    def iterate(self, global_best, omega=1, phip=1, phig=1):
+        self.velocity = omega * self.velocity + phip * (self.p_bitmap - self.x_bitmap) + phig * (
+                global_best - self.x_bitmap)
+        print(self.velocity)
         self.calculate_new_x()
         self.x_target = self.target_func(self.x_bitmap)
         if self.x_target > self.p_target:
             self.p_target = self.x_target
             self.p_target = np.copy(self.x_target)
-
-    def calculate_velocity(self, global_best, omega=0.5, phip=0.5, phig=0.5):
-        self.velocity = omega * self.velocity + phip * (self.p_bitmap - self.x_bitmap) + phig * (
-                global_best - self.x_bitmap)
-        # print(self.velocity)
 
     def calculate_new_x(self):
         for i in range(self.dimension):
