2ec339dd9eec4cea3f299f71c53c57a0653dd9f9
kacpersyn <kacper@student.agh.edu.pl>
2018-01-06 19:57:03 +0100

    New idea

--- particle.py
+++ particle.py
@@ -4,54 +4,38 @@ from accesspoint import *
 ##For more than one type of accesspoint
 
 class Particle:
-    def __init__(self, dimension, space, costs, x_size=g.COLS, y_size=g.ROWS):
-        self.x_size = x_size
-        self.y_size = y_size
-        self.space = space
-        self.costs = costs
-        # pointer to accesspoint types
-        # great idea
-        self.dimension = dimension
-        self.result_area = np.zeros((x_size, y_size))
-        self.x_bitmap = np.zeros((dimension, x_size, y_size))
-        self.p_bitmap = np.zeros((dimension, x_size, y_size))
-        self.velocity = np.zeros((dimension, x_size, y_size))
-        self.x_target = 0.0
-        self.p_target = 0.0
+    def __init__(self, target_func, lb, up):
+        self.x_size = target_func.x_size
+        self.y_size = target_func.y_size
+        self.dimension = target_func.dimension
+        self.target_func = target_func
+        self.x_bitmap = np.random.randint(lb, up + 1, size=(self.dimension, self.x_size, self.y_size))
+        self.velocity = np.random.randint(-abs(up - lb), abs(up - lb) + 1,
+                                          size=(self.dimension, self.x_size, self.y_size))
+        self.p_bitmap = np.copy(self.x_bitmap)
+        self.x_target = self.target_func(self.x_bitmap)
+        self.p_target = self.x_target
 
-    def generate_first_individual(self, number_of_aps):
-        for i in range(self.dimension):
-            temp_array = np.zeros(self.x_size * self.y_size)
-            print(number_of_aps[i])
-            temp_array[:number_of_aps[i]] = 1
-            np.random.shuffle(temp_array)
-            self.x_bitmap[i] = temp_array.reshape(self.x_size, self.y_size)
+    def get_local_best(self):
+        return self.p_bitmap, self.p_target
 
-    def update_result_area(self):
-        for i in range(self.dimension):
-            self.space[i].update_result_area(self.x_bitmap[i], self.result_area)
-
-    def target_function(self, users_area):
-        self.update_result_area()
-        total_aps = np.sum(self.x_bitmap, axis=(1, 2))
-        total_cost = total_aps.dot(self.costs)
-        new_area = users_area - self.result_area
-        self.x_target = np.sum(new_area[np.where(new_area > 0)]) - total_cost
+    def iterate(self, global_best, omega=0.5, phip=0.5, phig=0.5):
+        self.calculate_velocity(global_best, omega, phip, phig)
+        self.calculate_new_x()
+        self.x_target = self.target_func(self.x_bitmap)
         if self.x_target > self.p_target:
-            self.p_bitmap = np.copy(self.x_bitmap)
             self.p_target = self.x_target
-        return self.p_target
+            self.p_target = np.copy(self.x_target)
 
-    def calculate_velocity(self, global_best_bitmap, omega=1, fi_p=1, fi_g=1):
-        self.velocity = omega * self.velocity + fi_p * (self.p_bitmap - self.x_bitmap) + fi_g * (
-                global_best_bitmap - self.x_bitmap)
+    def calculate_velocity(self, global_best, omega=0.5, phip=0.5, phig=0.5):
+        self.velocity = omega * self.velocity + phip * (self.p_bitmap - self.x_bitmap) + phig * (
+                global_best - self.x_bitmap)
+        # print(self.velocity)
 
     def calculate_new_x(self):
-        probability_bitmap = self.x_bitmap + self.velocity
-        return probability_bitmap
-
-    def get_result_area(self):
-        return self.result_area
-
-    def get_ap_bitmap(self):
-        return self.x_bitmap
+        for i in range(self.dimension):
+            new = self.x_bitmap[i] + self.velocity[i]
+            probability = (new - new.min()) / (new.max() - new.min())
+            self.x_bitmap[i] = np.array([np.random.choice([0, 1], p=[1 - i, i]) for i in
+                                         probability.reshape(self.x_size * self.y_size)]).reshape(
+                (self.x_size, self.y_size))
--- targetfunc.py
+++ targetfunc.py
@@ -11,13 +11,13 @@ class Target:
         self.access_points = [AccessPoint(**x) for x in access_points_types]
         self.costs = np.array([x.cost for x in self.access_points])
         self.users_area = people
+        self.result_area = np.zeros((self.x_size, self.y_size))
 
     def __call__(self, multidimensional_bitmap):
-        result_area = np.zeros((self.x_size, self.y_size))
         for i in range(self.dimension):
-            self.access_points[i].update_result_area(multidimensional_bitmap[i], result_area)
+            self.access_points[i].update_result_area(multidimensional_bitmap[i], self.result_area)
         total_aps = np.sum(multidimensional_bitmap, axis=(1, 2))
         total_cost = total_aps.dot(self.costs)
-        new_area = self.users_area - result_area
+        new_area = self.users_area - self.result_area
         target = np.sum(new_area[np.where(new_area > 0)]) - total_cost
         return target
