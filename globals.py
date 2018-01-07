f8d8a0a3da2a26b6003ecacc95673752fc4a239d
kacpersyn <kacper@student.agh.edu.pl>
2018-01-07 19:45:59 +0100

    Nice

--- globals.py
+++ globals.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 COLS = 20
 ROWS = 20
 
@@ -9,3 +11,7 @@ access_points_types = [{'cost': 100, 'max_power': 100, 'access_area_fun': lambda
                                                                                  y: x * x + y * y <= 16,
                         'dist_fun': lambda diff_x, diff_y: (diff_x * diff_x + diff_y * diff_y).astype(float)}
                        ]
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
