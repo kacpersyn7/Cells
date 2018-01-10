c0b2fd737e2f0633883989d43daeb047d439107b
kacpersyn <kacper@student.agh.edu.pl>
2018-01-10 22:37:01 +0100

    POTEŻNA OPTYMALIZACJA

--- globals.py
+++ globals.py
@@ -1,15 +1,11 @@
 import numpy as np
 
-COLS = 20
-ROWS = 20
+COLS = 50
+ROWS = 50
 
-access_points_types = [{'cost': 100, 'max_power': 100, 'access_area_fun': lambda x,
-                                                                                 y: x * x + y * y <= 9,
-                        'dist_fun': lambda diff_x, diff_y: (diff_x * diff_x + diff_y * diff_y).astype(float)},
+access_points_types = [{'cost': 100, 'max_power': 100, 'radius': 3},
 
-                       {'cost': 213, 'max_power': 120, 'access_area_fun': lambda x,
-                                                                                 y: x * x + y * y <= 16,
-                        'dist_fun': lambda diff_x, diff_y: (diff_x * diff_x + diff_y * diff_y).astype(float)}
+                       {'cost': 213, 'max_power': 120, 'radius': 4}
                        ]
 
 
--- script.py
+++ script.py
@@ -1,7 +1,7 @@
-from particle import *
 from targetfunc import *
-from pso import *
+from targetfunc import *
+
 people = np.zeros((g.COLS, g.ROWS))
 people[10][10] = 40
 my_func = Target(people)
-best_bitmap, best_value = pso(my_func, 0, 390)
+best_bitmap, best_value = pso(my_func, 0, 2000, 200, 1, 1, 1, 5000)
