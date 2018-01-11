9c566136104d68f1af88d59e8b41cfce5024e437
kacpersyn <kacper@student.agh.edu.pl>
2018-01-11 22:41:42 +0100

    small corrections

--- pso.py
+++ pso.py
@@ -1,5 +1,6 @@
-from particle import *
 import time
+
+from particle import *
 
 
 def pso(func, lb=0, up=7000, swarm_size=200, omega=1, phip=1, phig=1, maxiter=500, start_time=time.time()):
@@ -7,12 +8,12 @@ def pso(func, lb=0, up=7000, swarm_size=200, omega=1, phip=1, phig=1, maxiter=50
     particles = np.array([Particle(func, x_randoms[x]) for x in range(swarm_size)])
     # We have particles
     g_iter = max(range(len(particles)), key=lambda i: particles[i].p_target)
-    g_array, g_target = particles[g_iter].get_local_best()
+    g_bitmap, g_target = particles[g_iter].get_local_best()
     for j in range(maxiter):
         for particle in particles:
-            a, b = particle.iterate(g_array, g_target, omega, phip, phig)
-            if a is not None:
-                g_array, g_target = a, b
+            p_bitmap, p_target = particle.iterate(g_bitmap, omega, phip, phig)
+            if p_target > g_target:
+                g_bitmap, g_target = p_bitmap, p_target
         #f j%100 == 0:
         print("Czas wykonania to:",(time.time()-start_time))
         start_time=time.time()
