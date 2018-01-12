e155109c8290e3a6a6c5c2fc3e573b9671f477c5
kacpersyn <kacper@student.agh.edu.pl>
2018-01-12 16:53:15 +0100

    no juz nie wiem jak nazwac

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from pso import *
 from targetfunc import *
 
@@ -5,4 +7,10 @@ people = np.zeros((g.COLS, g.ROWS))
 people[10][10] = 40
 #people[40][40] = 40
 my_func = Target(people)
-best_bitmap, best_value = pso(my_func, 0, 900, 800, 1, 1, 1, 5000)
+best_bitmap, best_value = pso(my_func, 0, 900, 800, 1, 1, 1, 1000)
+
+x, y = np.where(best_bitmap[1] == 1)
+
+plt.plot(x, y, 'ro')
+plt.axis([0, 49, 0, 49])
+plt.show()
