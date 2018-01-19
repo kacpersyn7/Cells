e8f6062bba3a023ad51d08ebaca064ddc6eb0cc7
kacpersyn <kacper@student.agh.edu.pl>
2018-01-19 01:37:59 +0100

    Zaczynam komentowac, no i zrobilem drugiego Funktora xD

--- accesspoint.py
+++ accesspoint.py
@@ -1,11 +1,8 @@
 import numba
 import numpy as np
 
-import globals as g
-
 
 @numba.jit(nopython=True, cache=True)
-# @numba.vectorize([numba.types.Array(numba.float64,2,'C')(numba.types.Array(numba.float64,2,'C'),numba.int64,numba.types.Array(numba.float64,2,'C'),numba.int64,numba.int64,numba.types.Array(numba.float64,2,'C'))])
 def update_result_area_outer(ap_bitmap, radius, result_area, x_size, y_size, circle):
     access_points = np.where(ap_bitmap == 1)
     for x, y in zip(access_points[0], access_points[1]):
@@ -29,24 +26,19 @@ def update_result_area_outer(ap_bitmap, radius, result_area, x_size, y_size, cir
 
 
 class AccessPoint:
-    def __init__(self, cost=100, max_power=10, radius=16, access_area_fun=lambda point: point[0] ** 2 + point[1] ** 2,
-                 x_size=g.COLS,
-                 y_size=g.ROWS):
+    """Klasa odpowiadająca za tworzenie różnych typów accesspointów. Jako argumenty przyjmuje:
+    koszt, maksymalną moc, zasięg, funkcję mocy w zależności od odległości euklidesowej.
+    Jest funktorem - wywołanie aktualizuje przestrzen rozwiazan."""
+
+    def __init__(self, cost=100, max_power=10, radius=16, access_area_fun=lambda point: point[0] ** 2 + point[1] ** 2):
         self.max_power = max_power
         self.access_area_fun = access_area_fun
         self.cost = cost
-        self.x_size = x_size
-        self.y_size = y_size
         self.radius = radius
         self.circle = self.init_circle()
 
     def get_power(self, dist):
         return (1. / dist) * self.max_power
-
-    def update_result_area(self, ap_bitmap, result_area):
-        result_area = update_result_area_outer(ap_bitmap, self.radius, result_area, self.x_size, self.y_size,
-                                               self.circle)
-        return result_area
 
     def init_circle(self):
         circle = np.copy(
@@ -58,23 +50,10 @@ class AccessPoint:
         circle[circle < 0] = 0
         return circle
 
-    """def update_result_area(self, ap_bitmap, result_area):
-        access_points = np.where(ap_bitmap == 1)
-        for x, y in zip(access_points[0], access_points[1]):
-            diff_x = self.radius - x
-            diff_y = self.radius - y
-            diff_x_2 = -diff_x
-            diff_y_2 = -diff_y
-            if diff_x < 0:
-                diff_x = 0
-            else:
-                diff_x_2 = 0
-            if diff_y < 0:
-                diff_y = 0
-            else:
-                diff_y_2 = 0
+    """Aktualizacja przestrzeni rozwiązań dla tych parametrów."""
 
-            result_area[diff_x_2:(x + self.radius + 1), diff_y_2:(y + self.radius + 1)] = np.maximum(
-                result_area[diff_x_2:(x + self.radius + 1), diff_y_2:(y + self.radius + 1)],
-                (self.circle[diff_x:(self.radius + self.x_size - x), diff_y:(self.radius + self.y_size - y)]))
-        return result_area"""
+    def __call__(self, ap_bitmap, result_area):
+        result_area = update_result_area_outer(ap_bitmap, self.radius, result_area, result_area.shape[0],
+                                               result_area.shape[1],
+                                               self.circle)
+        return result_area
--- main.py
+++ main.py
@@ -1,54 +1,16 @@
-import numpy as np
+from pso import *
+from targetfunc import *
 
-COLS = 50
-ROWS = 50
+if __name__ == "__main__":
+    resolution = (50, 50)
+    people1 = np.zeros(resolution)
+    people1[20][20] = 1
+    access_points_types = [
+        {'cost': 100, 'max_power': 100, 'radius': 3, 'access_area_fun': lambda point: point[0] ** 2 + point[1] ** 2},
 
+        {'cost': 213, 'max_power': 120, 'radius': 4, 'access_area_fun': lambda point: point[0] ** 2 + point[1] ** 2},
 
-def generate_area(x, y, resolution):
-    area = np.zeros((x, y))
-    return area
+    ]
 
-
-def get_power(ap_x, ap_y, ue_x, ue_y, max_power):
-    distance = ((ue_x - ap_x) ** 2 + (ue_y - ap_y) ** 2).astype(float)
-    distance[np.where(distance == 0)[0]] = 0.5
-    print(distance)
-    return (1. / (distance)) * max_power
-
-
-def update_result_area(accesspoint_bitmap, result_area, cols=COLS, rows=ROWS, max_power=400,
-                       activation_area=lambda x, y: x * x + y * y <= 16):
-    accesspoint_ones = np.where(accesspoint_bitmap == 0)
-    new_result_area = np.copy(result_area)
-    for x, y in zip(accesspoint_ones[0], accesspoint_ones[1]):
-        temp_y, temp_x = np.ogrid[-x:cols - x, -y:rows - y]
-        mask_indexes = np.where(activation_area(temp_x, temp_y))
-        print(mask_indexes)
-        new_result_area[mask_indexes] = get_power(x, y, mask_indexes[0], mask_indexes[1], max_power)
-        diff = np.where((new_result_area - result_area) < 0)
-        new_result_area[diff] = result_area[diff]
-        result_area = np.copy(new_result_area)
-    return new_result_area
-
-
-def generate_individuals(n=10, cols=COLS, rows=ROWS):
-    individuals = np.zeros((n, cols, rows))
-    for i in range(n):
-        individuals[i] = (np.random.randint(0, 10, size=(cols, rows)))
-    return individuals
-
-
-def generate_users(max_request, cols=COLS, rows=ROWS):
-    return np.random.randint(0, max_request, size=(cols, rows))
-
-
-def second_target_function(power_area, accesspoint_area, users_area, cost_of_accesspoint):
-    cost_function = np.sum(accesspoint_area == 0) * cost_of_accesspoint
-    print(np.sum(accesspoint_area == 0))
-    quality_function = np.sum(users_area * (power_area - users_area))
-    return quality_function - cost_function
-
-
-users = generate_users(250)
-aps = generate_individuals()
-power_a = np.zeros((COLS, ROWS))
+    fc1 = Target(people1, access_points_types)
+    g_bitmap, g_target = pso(fc1)
