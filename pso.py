94b1b126a053e249a1e2c3c07c97d6535af6351a
203a <braun.grzegorz.kosciol@gmail.com>
2018-01-21 14:09:14 +0100

    zapis wyniku do pliku

--- pso.py
+++ pso.py
@@ -1,7 +1,6 @@
 import time
-
+import multiprocessing
 from particle import *
-
 
 def pso(func, lb=0, up=500, swarm_size=100, omega=1, phip=1, phig=1, maxiter=2000, start_time=time.time()):
     x_randoms = np.random.randint(lb, up, size=(swarm_size, func.dimension))
@@ -14,8 +13,11 @@ def pso(func, lb=0, up=500, swarm_size=100, omega=1, phip=1, phig=1, maxiter=200
             if p_target > g_target:
                 g_target = p_target
                 g_bitmap = particle.get_p_bitmap()
-        print("Czas wykonania to:",(time.time()-start_time))
-        start_time=time.time()
-        print("iteracja ", str(j), " wynik ", str(g_target))
+        # pool = multiprocessing.Pool(4)
+        # g_target, g_bitmap = pool.map(iterate_particles, [g_bitmap, omega, phip, phig, particles])
+        if j%100 == 0:
+            print("Czas wykonania to:",(time.time()-start_time))
+            start_time=time.time()
+            print("iteracja ", str(j), " wynik ", str(g_target))
 
     return g_bitmap, g_target
--- script.py
+++ script.py
@@ -4,7 +4,7 @@ from pso import *
 from targetfunc import *
 
 def read_people_from_file(name, rows, cols):
-    """ Czyta bitmape ludzi z pliku, jako argument przyjmuje nazwe pliku i zwraca zczytana bitmapę"""
+    """ Czyta bitmape ludzi z pliku, jako argument przyjmuje nazwe pliku i zwraca zczytaną bitmapę"""
     people_heatmap = np.zeros((rows, cols))
     with open(name,'r') as file:
         for line in file:
@@ -20,22 +20,47 @@ def read_people_from_file(name, rows, cols):
                     people_heatmap[coords[0]][coords[1]] = power
     return people_heatmap
 
-def write2file(x,y):
-    with open("out.log",'w') as file:
+def write2file(x,y, index):
+    string2 = "out" + str(index)
+    with open(string2,'w') as file:
         for i in range(len(x)):
             string = "x: " + str(x[i]) + " y: " + str(y[i]) + "\n"
             file.write(string);
 
 
 if __name__ == "__main__":
-    people = read_people_from_file("template_people",g.ROWS,g.COLS)
+    people = read_people_from_file("rand_people",g.ROWS,g.COLS)
     my_func = Target(people)
-    best_bitmap, best_value = pso(my_func, 0, 300, 200, 1, 1, 1, 1000)
-
+    population = 500 #
+    omega = [0.9, 1, 1.1] #
+    phig = [0.9, 1, 1.1] #
+    phip = [0.9, 1, 1.1] #
+    min_rand = [0, 400, 1000]
+    max_rand = [1100, 1900, 2500]
+    max_iter = 1000
+    i = 0
+    start_time2137= time.time()
+    for om in omega:
+        for g in phig:
+            for p in phip:
+                for m_r in min_rand:
+                    for ma_r in max_rand:
+                        best_bitmap, best_value = pso(my_func, m_r, ma_r, population, om, p, g, max_iter)
+                        x, y = np.where(best_bitmap[1] == 1)
+                        write2file(x, y, i)
+                        i = i + 1
+    print("Calosc wykonywala sie: ", time.time()-start_time2137)
     x, y = np.where(best_bitmap[1] == 1)
     print(x)
     print(y)
     write2file(x,y)
     plt.plot(x, y, 'ro')
     plt.axis([0, g.COLS-1, 0, g.ROWS-1])
+    x, y = np.where(best_bitmap[0] == 1)
+    plt.plot(x, y, 'bx'
+                   '')
+    plt.axis([0, g.COLS-1, 0, g.ROWS-1])
+    plt.show()
+
+
     plt.show()
