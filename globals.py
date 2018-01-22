cb14eaa0f45acb043289f726d493588e292ebf2f
203a <braun.grzegorz.kosciol@gmail.com>
2018-01-22 22:57:01 +0100

    plik z kółkami

--- globals.py
+++ globals.py
@@ -1,7 +1,7 @@
 COLS = 50
 ROWS = 50
 
-access_points_types = [{'cost': 8000, 'max_power': 1000, 'radius': 3},
+access_points_types = [{'cost': 4000, 'max_power': 1000, 'radius': 4},
 
-                       {'cost': 10000, 'max_power': 1200, 'radius': 2}
+                       {'cost': 5000, 'max_power': 1200, 'radius': 3}
                        ]
