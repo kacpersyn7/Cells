db5a29fe7ae4a2bd5921bd6cdc98d7cd748c4b7b
kacpersyn <kacper@student.agh.edu.pl>
2018-01-22 23:09:05 +0100

    Zmiana na sigmoide - mysle, że realniejsza funkcja celu.

--- targetfunc.py
+++ targetfunc.py
@@ -1,23 +1,30 @@
 from particle import *
-from userequipment import *
+
+
+@jit(nopython=True, parallel=True)
+def sigmoid(x, L=200, k=10, x0=10):
+    return L / (1 + np.exp(-k * (x - x0)))
 
 
 class Target:
     """Piszę po polsku, bo mało czasu. Klasa odpowiadająca za funkcję celu.
        Jako argumenty otrzymuje przestrzeń wymagań (ludzie),
-       typy accesspointow(odpowiadajace za aktualizacje przestrzeni rozwiazan)
-       oraz 2 funkcje: pierwsza będąca funkcją lub współczynnikiem dla wagi przestrzeni wymagań, druga wagi kosztów"""
+       typy accesspointow(odpowiadajace za aktualizacje przestrzeni rozwiazan),
+       parametry - wspolczynnik wymagań, kosztów oraz parametry sigmoidy
+       dzialajacej na roznice miedzy wymaganiami a sila sygnalu"""
 
     def __init__(self, people=np.random.randint(0, 20, size=(g.COLS, g.ROWS)),
-                 access_points_types=g.access_points_types, people_function=lambda x: 10 * x,
-                 cost_function=lambda x: 4*x):
+                 access_points_types=g.access_points_types, cost_factor=1, people_factor=1, L=200, k=10, x0=10):
         self.shape = people.shape
         self.dimension = len(access_points_types)
         self.access_points = [AccessPoint(**x) for x in access_points_types]
         self.costs = np.array([x.cost for x in self.access_points])
         self.users_area = people
-        self.people_function = people_function
-        self.cost_function = cost_function
+        self.cost_factor = cost_factor
+        self.people_factor = people_factor
+        self.L = L
+        self.k = k
+        self.x0 = x0
 
     def get_result_area(self, multidimensional_bitmap):
         result_area = np.zeros(self.shape)
@@ -25,10 +32,12 @@ class Target:
             result_area = self.access_points[i](multidimensional_bitmap[i], result_area)
         return result_area
 
-    """Wywołanie funkcji celu dla wielowymiarowej bitmapy             czyli? accesspointow?? tylko obszaru zasiegów???          """
+    """Wywołanie funkcji celu dla wielowymiarowej bitmapy accesspointow"""
+
     def __call__(self, multidimensional_bitmap):
         total_aps = np.sum(multidimensional_bitmap, axis=(1, 2))
         total_cost = total_aps.dot(self.costs)
         new_area = self.get_result_area(multidimensional_bitmap) - self.users_area
-        target = np.sum(new_area[np.where(self.users_area > 0)]) - total_cost
+        target = self.people_factor * np.sum(
+            self.users_area * sigmoid(new_area, self.L, self.k, self.x0)) - self.cost_factor * total_cost
         return target
