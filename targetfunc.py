534e18e6ee716690d3006f05110c3bdcd9460172
kacpersyn <kacper@student.agh.edu.pl>
2018-01-23 01:16:40 +0100

    Poprawiłem lekko funkcję celu.

--- targetfunc.py
+++ targetfunc.py
@@ -1,5 +1,6 @@
-from particle import *
 import globals as g
+from particle import *
+
 
 @jit(nopython=True, parallel=True)
 def sigmoid(x, L=200, k=10, x0=10):
@@ -14,7 +15,7 @@ class Target:
        dzialajacej na roznice miedzy wymaganiami a sila sygnalu"""
 
     def __init__(self, people=np.random.randint(0, 20, size=(g.COLS, g.ROWS)),
-                 access_points_types=g.access_points_types, cost_factor=1, people_factor=1, L=200, k=10, x0=10):
+                 access_points_types=g.access_points_types, cost_factor=1, people_factor=1, L=300, k=10, x0=10):
         self.shape = people.shape
         self.dimension = len(access_points_types)
         self.access_points = [AccessPoint(**x) for x in access_points_types]
@@ -22,6 +23,7 @@ class Target:
         self.users_area = people
         self.cost_factor = cost_factor
         self.people_factor = people_factor
+        self.maximum = np.max(self.users_area)
         self.L = L
         self.k = k
         self.x0 = x0
@@ -39,5 +41,6 @@ class Target:
         total_cost = total_aps.dot(self.costs)
         new_area = self.get_result_area(multidimensional_bitmap) - self.users_area
         target = self.people_factor * np.sum(
-            self.users_area * sigmoid(new_area, self.L, self.k, self.x0)) - self.cost_factor * total_cost
+            (self.users_area / self.maximum) * sigmoid(new_area, self.L, self.k,
+                                                       self.x0)) - self.cost_factor * total_cost
         return target
