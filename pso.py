1aa79706ef44708725e33c04b5dda0f0c4010dc0
kacpersyn <kacper@student.agh.edu.pl>
2018-01-24 22:49:25 +0100

    refraktoryzacja (powiedzmy xd)

--- pso.py
+++ pso.py
@@ -17,8 +17,8 @@ def pso(func, lb=0, up=500, swarm_size=100, omega=1, phip=1, phig=1, maxiter=200
                 g_target = p_target
                 g_bitmap = particle.get_p_bitmap()
                 results.append((j + 1, g_bitmap, g_target))
-        # if j % 10 == 0:
-        #     print("Czas wykonania to:", (time.time() - start_time))
-        #     start_time = time.time()
-        #     print("iteracja ", str(j), " wynik ", str(g_target))
+        if j % 10 == 0:
+            print("Czas wykonania to:", (time.time() - start_time))
+            start_time = time.time()
+            print("iteracja ", str(j), " wynik ", str(g_target))
     return results
