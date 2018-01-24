54dcfd0b3a09b90b9f88bb575e87c2d0023c6ed2
203a <braun.grzegorz.kosciol@gmail.com>
2018-01-24 19:56:31 +0100

    test zawiera funkcje do generowania ludzi i pisania, a czytanie do czytanie wynikow ze script

--- globals.py
+++ globals.py
@@ -1,7 +1,7 @@
 COLS = 50
 ROWS = 50
 
-access_points_types = [{'cost': 4000, 'max_power': 1000, 'radius': 4},
+access_points_types = [{'cost': 2500, 'max_power': 1000, 'radius': 8},
 
-                       {'cost': 5000, 'max_power': 1200, 'radius': 3}
+                       {'cost': 5000, 'max_power': 1500, 'radius': 12}
                        ]
