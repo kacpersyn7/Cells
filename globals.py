d030135b73227b5662bf461c548573a865fc11ad
kacpersyn <kacper@student.agh.edu.pl>
2018-01-25 01:22:20 +0100

    Graphics, to 3 proste funkcje do wyswietlania - bitmapy, heatmapy i funkcji celu. Dodatkowo i/o przenioslem do jednego pliku test także

--- globals.py
+++ globals.py
@@ -1,7 +1,7 @@
 COLS = 50
 ROWS = 50
 
-access_points_types = [{'cost': 2500, 'max_power': 1000, 'radius': 8},
+access_points_types = [{'cost': 2500, 'max_power': 500, 'radius': 16},
 
-                       {'cost': 5000, 'max_power': 1500, 'radius': 12}
+                       {'cost': 5000, 'max_power': 1000, 'radius': 25}
                        ]
